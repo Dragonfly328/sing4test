c4e34cdf75376dbcab518e92a071eb4b08c6fd3e
Jacob <otteja01@luther.edu>
2011-06-28 21:23:53 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sing4test::Application.config.secret_token = '47d5504ada02261ef12cb44ef32b0cba200281240701d75eb87a5e47257b24b67eb12dc899c2bee8f5592538b4283e852d9687723114ee196d1b9ce59ea84a7c'
